ffe85363b37fa21ed400b2c2fa147d00f7e3aa72
THEBIGTOMMY13DEV <thebigtommy13tech@gmail.com>
2026-01-09 11:47:18 -0500

    first commit

---
+++ Playlists/Playlist Creator/backendplaylistcreator.js
@@ -0,0 +1,48 @@
+// console.log("Data Loading ...");
+// var xhttp = new XMLHttpRequest();
+
+// xhttp.onreadystatechange = function() {
+//     if (this.readyState == 4 && this.status == 200) {
+//         let playlist = JSON.parse(xhttp.responseText);
+//         console.log(playlist);
+    
+//     playlist.forEach(function(SpotifySongs){
+//         let Songs = document.createElement("div");
+//         Songs.classList.add("Songs");
+//     let textData=`<img src="${albumImage}" alt="${albumName}"><h3>${songName}</h3> <p><strong>Artist:</strong> ${artistName}</p> <p><strong>Album:</strong> ${albumName}</p> <p><strong>Year:</strong> ${releaseYear}</p>`;
+//     Songs.innerHTML=textData;
+//     container.appendChild(Songs);
+//     });
+//  }
+// };
+// xhttp.open("GET", "Spotify.json", true);
+// xhttp.send();
+
+const xhr = new XMLHttpRequest();
+xhr.open("GET", "Spotify.json", true);
+
+xhr.onreadystatechange = function () {
+  if (xhr.readyState === 4 && xhr.status === 200) {
+    const data = JSON.parse(xhr.responseText);
+
+    const trackList = document.getElementById("track-list");
+
+    const song = data.track.name;
+    const artist = data.artists.map(a => a.name).join(", ");
+    const album = data.album.name;
+
+    const row = document.createElement("div");
+    row.className = "track-item";
+
+    row.innerHTML = `
+      <div class="track-text">
+        ${song}, ${artist}, ${album}
+      </div>
+      <button class="add-btn">+</button>
+    `;
+
+    trackList.appendChild(row);
+  }
+};
+
+xhr.send();
