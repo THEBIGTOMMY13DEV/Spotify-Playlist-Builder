aa2a3e49e594b26793b13bb06a84390efffe5a20
THEBIGTOMMY13DEV <thebigtommy13tech@gmail.com>
2026-01-28 12:17:51 -0500

    Added Form Page

--- Playlists/Playlist Creator/backendplaylistcreator.js
+++ Playlists/Playlist Creator/backendplaylistcreator.js
@@ -1,23 +1,3 @@
-// console.log("Data Loading ...");
-// var xhttp = new XMLHttpRequest();
-
-// xhttp.onreadystatechange = function() {
-//     if (this.readyState == 4 && this.status == 200) {
-//         let playlist = JSON.parse(xhttp.responseText);
-//         console.log(playlist);
-    
-//     playlist.forEach(function(SpotifySongs){
-//         let Songs = document.createElement("div");
-//         Songs.classList.add("Songs");
-//     let textData=`<img src="${albumImage}" alt="${albumName}"><h3>${songName}</h3> <p><strong>Artist:</strong> ${artistName}</p> <p><strong>Album:</strong> ${albumName}</p> <p><strong>Year:</strong> ${releaseYear}</p>`;
-//     Songs.innerHTML=textData;
-//     container.appendChild(Songs);
-//     });
-//  }
-// };
-// xhttp.open("GET", "Spotify.json", true);
-// xhttp.send();
-
 const xhr = new XMLHttpRequest();
 xhr.open("GET", "Spotify.json", true);
 
@@ -29,7 +9,7 @@ xhr.onreadystatechange = function () {
     const Playlist = document.getElementById("Song");
     const row = document.createElement("div");
     row.className = 'Song-item';
-    row.innerHTML = "<div class='Song-text'>" +songs.TrackName + ", "+songs.Artists+"</div>"+"<button class='PlaylistaddButton'>+</button>";
+    row.innerHTML = "<div class='Song-text'>" +songs.Song + ", "+songs.Artists+"</div>"+"<button class='PlaylistaddButton'>+</button>";
     Playlist.appendChild(row);
   });
     document.querySelectorAll(".PlaylistaddButton").forEach(PlaylistBTN => {
